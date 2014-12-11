560fc8aaad6f997046f2fc954f5af898d1240a6c
binneo20 <sweetestbinbin@hotmail.com>
2014-12-10 20:05:36 -0500

    Update main.js

--- hw_assignment3/js/main.js
+++ hw_assignment3/js/main.js
@@ -1,15 +1,15 @@
 function squareNumber(numb) {
 
-	return numb * numb
+	return numb * numb;
 }
 
 function calculatorSquarePrompts(){
 
 var numb = prompt('Square a number.');
 
-var square=squareNumber(numb)
+var square=squareNumber(numb);
 
-alert('The result of squaring the number ' + numb + ' is ' + square)
+alert('The result of squaring the number ' + numb + ' is ' + square);
 
 }
 
@@ -17,33 +17,33 @@ alert('The result of squaring the number ' + numb + ' is ' + square)
 
 function halfNumb(numb) {
 
-	return numb / 2
+	return numb / 2;
 
 }
 
 function calculatorHalfPrompts(){
 
-	var numb=prompt('Divide a number by half.')
-	var half=halfNumb(numb)
-	alert('Half of ' + numb + ' is ' + half + '.')
+	var numb=prompt('Divide a number by half.');
+	var half=halfNumb(numb);
+	alert('Half of ' + numb + ' is ' + half + '.');
 }
 
 ///
 
 function percentOf(numb1, numb2) {
 
-	return numb1 / numb2 * 100
+	return numb1 / numb2 * 100;
 }
 
 function calculatePercentPrompts(){
 
-	var numb1=prompt('Start with a first number.')
+	var numb1=prompt('Start with a first number.');
 
-	var numb2=prompt('Start with a second number.')
+	var numb2=prompt('Start with a second number.');
 
-	var percent=percentOf(numb1,numb2)
+	var percent=percentOf(numb1,numb2);
 
-	alert(numb1 + ' is ' + percent +'% of ' + numb2 +'.')
+	alert(numb1 + ' is ' + percent +'% of ' + numb2 +'.');
 
 }
 
@@ -52,15 +52,15 @@ function calculatePercentPrompts(){
 
 function areaOfCircle(radius) {
 
-	return Math.PI * 2 * radius
+	return Math.PI * 2 * radius;
 
 }
 
 function calculateAreaPrompts() {
 
-	var radius=prompt('Enter the radius of a circle.')
+	var radius=prompt('Enter the radius of a circle.');
 
-	var area=areaOfCircle(radius)
+	var area=areaOfCircle(radius);
 
 	var finalArea = area.toFixed(2);
 
@@ -70,37 +70,19 @@ function calculateAreaPrompts() {
 
 ////
 
-function halfNumb(numb) {
 
-	return numb / 2;
-
-}
-
-function squareNumber(half) {
-
-	var half=halfNumb(numb);
-
-	return half * half;
-}
-
-function areaOfCircle(square) {
-
-	var square=squareNumber(half);
-
-	return Math.PI * 2 * square;
-
-}
-
-function percentOf(finalArea, square) {
-	var square=squareNumber(half);
-	var area=areaOfCircle(square);
-	var finalArea = area.toFixed(2);
-	return finalArea / square * 100;
-}
 
 function calculateFinalPrompts() {
 
 	var numb=prompt('Start with a number.');
+
+	var half=halfNumb(numb);
+
+	var square=squareNumber(half);
+
+	var area= areaOfCircle(square);
+
+	var finalArea = area.toFixed(2);
 
 	var percent=percentOf(finalArea,square);
 
